0507269805415d27c9a916cf9b0cd6738cdcc3c2
Mayra Valdes <4valdes@gmail.com>
2019-07-31 23:17:31 +0100

    Add plot for density in covariates

--- src/_eda/correlations.R
+++ src/_eda/correlations.R
@@ -1,20 +1,51 @@
 library(RColorBrewer)
 
-pattern <- paste(config$predictors$valid_suffixes, collapse="|")
-importants <- filterVarImp(x = all[,which(grepl(pattern, names(all)))], y = all$response_factor)
+# pattern <- paste(config$predictors$valid_suffixes, collapse="|")
+# importants <- filterVarImp(x = all[,which(grepl(pattern, names(all)))], y = all$response_factor)
 
-importants <- importants[order(-importants$yes),]
-importants <- importants[1:length(predictors),]
-importants_names <- rownames(importants)
+# importants <- importants[order(-importants$yes),]
+# importants <- importants[1:length(predictors),]
+# importants_names <- rownames(importants)
+
+key.trans <- list(
+  space="bottom", 
+  columns=2,
+  text=list(config$theme$parties_labels),
+  lines=list(col=config$theme$parties_colors),
+  cex.title=1, 
+  cex=.9,
+  title="Winning Party"
+)
+
+png(filename="figures/density_plots.png", width=300, height=200, bg="white", unit="mm", res=300)
 
 featurePlot(
+  # to change the order of plots
+  index.cond=list(c(14:1,NULL)),
   x=as.data.frame(train.data[, which(colnames(all) %in% predictors[predictors != "response_regression"])]), 
   y=pull(train.data[, which(colnames(all) %in% "response_factor")]), 
-  plo ="density",
+  plo="density",
+  # theme settings
+  pch=16, # what's the figure for the density
+  cex=0.2,  # what's the size for the figure
+  col=config$theme$parties_colors,
+  par.settings=list(
+    strip.background=list(col="#f3f3f3")
+  ),
+  strip=strip.custom(par.strip.text=list(cex=.6)),
+  # titles
+  main="Density plots for each covariate and winning party",
+  labels=c(),
+  # scales for each plot axis
   scales=list(x=list(relation="free"), y=list(relation="free")),
-  strip=strip.custom(par.strip.text=list(cex=.7)),
-  auto.key=list(space="top", columns=2, cex.title=1)
+  # legend
+  key=key.trans,
+  layout=c(5, 3)
 )
+
+dev.off()
+
+############################
 
 temp_predictors <- predictors
 temp_predictors[temp_predictors=="response_regression"] <- "frac_republican"
