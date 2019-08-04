88d2e1e70da178d41da93a53e5f2518e1e854b74
Mayra Valdes <4valdes@gmail.com>
2019-08-04 15:32:13 +0100

    Add tuning and seeds

--- src/_models/methods.R
+++ src/_models/methods.R
@@ -1,17 +1,10 @@
-
-# length is = (nresampling)+1
-seeds <- vector(mode = "list", length = nrow(train.data) + 1)
-seeds <- lapply(seeds, function(x) {
-  1:20
-})
-seeds[[nrow(train.data) + 1]] <- 1 # 1 for the last one
+set.seed(config$seed)
 
 control <- trainControl(
   method="cv", 
-  number=10, 
+  number=config$n_resampling, 
   classProbs=TRUE, 
   summaryFunction=twoClassSummary, 
-  seeds=seeds,
   allowParallel=FALSE
 )
 
@@ -38,14 +31,15 @@ my_methods <- list(
     method="glm",
     preProc=c()
   ),
-  "glm_aic" = list(
-    method="glmStepAIC",
-    name="LR Backward Elimination (AIC)"
-  ),
-  "glm_bic" = list(
-    method="glmStepAIC",
-    name="LR Backward Elimination (BIC)"
-  ),
+  #"glm_aic" = list(
+  #  method="glmStepAIC",
+  #  name="LR Backward Elimination (AIC)"
+  #),
+  #"glm_bic" = list(
+  #  method="glmStepAIC",
+  #  name="LR Backward Elimination (BIC)"
+  #),
+  # https://stats.stackexchange.com/questions/48360/is-standardization-needed-before-fitting-logistic-regression
   "glmnet" = list(
     name="Penalized Logistic Regression",
     tuneGrid=expand.grid(
@@ -53,7 +47,8 @@ my_methods <- list(
       alpha=seq(0.05, 0.95, by=0.05),
       lambda=seq(0.001, 0.01, by=0.001)
     ), 
-    optimizeGrid="glmnet"
+    optimizeGrid="glmnet",
+    tunes=9
   ),
   "rf" = list(
     name="Random Forest",
@@ -66,12 +61,32 @@ my_methods <- list(
     allowParallel=TRUE
   ),
   "svmLinear"=list(
-    name="SVM with Linear Kernel"
+    name="SVM (svmLinear)",
+    tuneGrid=expand.grid(
+      C=c(.25, .5, 1, 10, 100)
+    ),
+    optimizeGrid="svm"
   ),
   "svmPoly"=list(
-    name="SVM with Polynomial Kernel"
+    name="SVM (svmPoly)",
+    params=list(
+      tuneLength=4
+    ),
+    tuneGrid=expand.grid(
+      C=c(.01, .1, 1, 10, 100),
+      degree=c(1, 5, 10, 20),
+      scale=1
+    ),
+    optimizeGrid="svm",
+    tunes=38
   ),
   "svmRadial"=list(
-    name="Radial Basis Function Kernel"
+    name="SVM (svmRadial)",
+    tuneGrid=expand.grid(
+      C=c(.25, .5, 1),
+      sigma=sort(c(2^c(-15,-10, -5, 0), 0.05))
+    ),
+    optimizeGrid="svm",
+    tunes=5
   )
 )
