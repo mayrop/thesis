e7850644a1fa028c148cfd9f9243cbef3c9a612c
Mayra Valdes <4valdes@gmail.com>
2019-08-14 00:32:14 +0100

    Added namespaces

--- src/_eda/summaries.R
+++ src/_eda/summaries.R
@@ -1,32 +1,32 @@
 # First summarize the votes
 summary_votes <- elections_summary %>%
-  group_by(party) %>%
-  summarize(
+  dplyr::group_by(party) %>%
+  dplyr::summarize(
     votes = sum(votes) / 1e6
   )
 
 # Then summarize counties (excluding Alaska)
 summary_counties <- elections_summary %>%
-  group_by(party) %>%
-  filter(
+  dplyr::group_by(party) %>%
+  dplyr::filter(
     state_abbreviation != "AK"
   ) %>%
-  summarize(
+  dplyr::summarize(
     counties = sum(party_won == party)
   )
 
 # Then join both summaries
 summary <- summary_votes %>%
-  left_join(summary_counties, by="party") %>%
-  mutate(
+  dplyr::left_join(summary_counties, by="party") %>%
+  dplyr::mutate(
     votes_pct = votes / sum(votes) * 100,
     counties_pct = counties / sum(counties) * 100
   ) %>%
-  mutate_if(
+  dplyr::mutate_if(
     is.numeric, round, digits = 10
   ) %>%
-  unite(., "Popular Vote, (millions)", c("votes", "votes_pct")) %>%
-  unite(., "Counties Won[note]", c("counties", "counties_pct"))
+  tidyr::unite(., "Popular Vote, (millions)", c("votes", "votes_pct")) %>%
+  tidyr::unite(., "Counties Won[note]", c("counties", "counties_pct"))
 
 # transpose the data frame
 summary <- dcast(
@@ -54,14 +54,14 @@ summary %>%
 
 # Now let's summarize the facts...
 facts_summary <- all %>%
-  ungroup() %>%
-  mutate(
+  dplyr::ungroup() %>%
+  dplyr::mutate(
     party = ifelse(response_factor == "yes", "republican", "democratic")
   ) %>%
-  group_by(
+  dplyr::group_by(
     party
   ) %>%
-  select(
+  dplyr::select(
     party,
     pop_14,
     pop_10,
@@ -80,48 +80,48 @@ facts_summary <- all %>%
     inc_pc_12_month_13,
     age_o65_pct_14
   ) %>% 
-  mutate (
+  dplyr::mutate(
     pop_10 = pop_10 / 1e3,
     pop_14 = pop_14 / 1e3,
     hsg_val_units_13 = hsg_val_units_13 / 1e3
   ) %>% 
-  group_by(party) %>%
+  dplyr::group_by(party) %>%
   # this can be approached by skim_to_list (skimr package)
   # but we try to minimize the third party packages
-  summarise_all(
+  dplyr::summarise_all(
     list(mean = mean, median = median, sd = sd)
   ) %>%
-  gather(
+  tidyr::gather(
     "var", "val", -party
   ) %>%
-  mutate(
+  dplyr::mutate(
     stat = gsub("^[a-z0-9_]+_([a-z]+)$", "\\1", var),
     var = gsub("^([a-z0-9_]+)_[a-z]+$", "\\1", var)
   ) %>%
-  group_by(
+  dplyr::group_by(
     party, var
   ) %>% 
-  spread(
+  tidyr::spread(
     stat, val
   ) %>% 
-  group_by(var) %>%
-  unite(temp, mean, median, sd) %>%
-  spread(party, temp) %>%
-  separate(
+  dplyr::group_by(var) %>%
+  tidyr::unite(temp, mean, median, sd) %>%
+  tidyr::spread(party, temp) %>%
+  tidyr::separate(
     democratic, 
     into = paste("dem", c("mean", "median", "sd"), sep="_"), 
     sep = "_"
   ) %>%
-  separate(
+  tidyr::separate(
     republican, 
     into = paste("rep", c("mean", "median", "sd"), sep="_"), 
     sep = "_"
   ) %>%
-  mutate_at(
+  dplyr::mutate_at(
     vars(-1), as.numeric
   ) %>% 
-  ungroup() %>%
-  mutate_if(
+  dplyr::ungroup() %>%
+  dplyr::mutate_if(
     is.numeric, plyr::round_any, accuracy = .001, f = floor
   )
 
